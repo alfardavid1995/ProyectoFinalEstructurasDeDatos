1c7e2b96a9be557ece8935348dfc8ed64793500d
David Alfaro <alfardavid95@gmail.com>
2022-11-07 22:22:37 -0600

    solicitud de alquiler variables agregadas

--- ProyectoFinal_Estructuras/src/GUI/GUISolicitudAlquiler.java
+++ ProyectoFinal_Estructuras/src/GUI/GUISolicitudAlquiler.java
@@ -32,6 +32,8 @@ public class GUISolicitudAlquiler extends javax.swing.JFrame {
         jTextField1 = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(882, 550));
@@ -46,31 +48,41 @@ public class GUISolicitudAlquiler extends javax.swing.JFrame {
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
 
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jScrollPane2.setViewportView(jTextArea2);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(122, 122, 122)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(483, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(54, 54, 54))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(145, 145, 145)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(119, 119, 119)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 358, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(122, 122, 122)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addComponent(jLabel1)))
+                .addContainerGap(22, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(70, 70, 70)
+                .addGap(44, 44, 44)
                 .addComponent(jLabel1)
-                .addGap(29, 29, 29)
+                .addGap(55, 55, 55)
                 .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(83, 83, 83)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(98, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 331, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(50, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -126,7 +138,9 @@ public class GUISolicitudAlquiler extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea2;
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
--- ProyectoFinal_Estructuras/src/ObjetosdelProyecto/Objetos/SolicitudDeAlquiler.java
+++ ProyectoFinal_Estructuras/src/ObjetosdelProyecto/Objetos/SolicitudDeAlquiler.java
@@ -10,7 +10,8 @@ import EstructurasDeDatos.*;
  * @author css124646
  */
 public class SolicitudDeAlquiler {
-    private String estadoDeSolicitud;//Registrada, procesada, finalizada
+    private String estadoDeSolicitud;//Registrada, procesada, finalizada, rechazada
+    private int idSolicitud;
     private int cantidadDeDiasSolicitud;
     private Cliente usuarioSolicitud;
     private int cantidadMinimaPasajerosSolicitud;
@@ -21,6 +22,111 @@ public class SolicitudDeAlquiler {
     private double montoSolicitud;
     private Vehiculo vehiculoSolicitud;
     private ListaDeVehiculosDisponiblesParaAlquilar_SolicitudDeAlquiler listaDeVehiculos;
+
+    public SolicitudDeAlquiler(String estadoDeSolicitud, int idSolicitud, int cantidadDeDiasSolicitud, Cliente usuarioSolicitud,  Vehiculo vehiculoSolicitud) {
+        this.estadoDeSolicitud = estadoDeSolicitud;
+        this.idSolicitud = idSolicitud;
+        this.cantidadDeDiasSolicitud = cantidadDeDiasSolicitud;
+        this.montoSolicitud = montoSolicitud;
+        this.vehiculoSolicitud = vehiculoSolicitud;
+    }
+
+    public String getEstadoDeSolicitud() {
+        return estadoDeSolicitud;
+    }
+
+    public void setEstadoDeSolicitud(String estadoDeSolicitud) {
+        this.estadoDeSolicitud = estadoDeSolicitud;
+    }
+
+    public int getIdSolicitud() {
+        return idSolicitud;
+    }
+
+    public void setIdSolicitud(int idSolicitud) {
+        this.idSolicitud = idSolicitud;
+    }
+
+    public int getCantidadDeDiasSolicitud() {
+        return cantidadDeDiasSolicitud;
+    }
+
+    public void setCantidadDeDiasSolicitud(int cantidadDeDiasSolicitud) {
+        this.cantidadDeDiasSolicitud = cantidadDeDiasSolicitud;
+    }
+
+    public Cliente getUsuarioSolicitud() {
+        return usuarioSolicitud;
+    }
+
+    public void setUsuarioSolicitud(Cliente usuarioSolicitud) {
+        this.usuarioSolicitud = usuarioSolicitud;
+    }
+
+    public int getCantidadMinimaPasajerosSolicitud() {
+        return cantidadMinimaPasajerosSolicitud;
+    }
+
+    public void setCantidadMinimaPasajerosSolicitud(int cantidadMinimaPasajerosSolicitud) {
+        this.cantidadMinimaPasajerosSolicitud = cantidadMinimaPasajerosSolicitud;
+    }
+
+    public String getMarcaVehiculoSolicitud() {
+        return marcaVehiculoSolicitud;
+    }
+
+    public void setMarcaVehiculoSolicitud(String marcaVehiculoSolicitud) {
+        this.marcaVehiculoSolicitud = marcaVehiculoSolicitud;
+    }
+
+    public String getModelovehiculoSolicitud() {
+        return modelovehiculoSolicitud;
+    }
+
+    public void setModelovehiculoSolicitud(String modelovehiculoSolicitud) {
+        this.modelovehiculoSolicitud = modelovehiculoSolicitud;
+    }
+
+    public int getAnioVehiculoEnLaSolicitud() {
+        return anioVehiculoEnLaSolicitud;
+    }
+
+    public void setAnioVehiculoEnLaSolicitud(int anioVehiculoEnLaSolicitud) {
+        this.anioVehiculoEnLaSolicitud = anioVehiculoEnLaSolicitud;
+    }
+
+    public String getExtrasVehiculoSolicitud() {
+        return extrasVehiculoSolicitud;
+    }
+
+    public void setExtrasVehiculoSolicitud(String extrasVehiculoSolicitud) {
+        this.extrasVehiculoSolicitud = extrasVehiculoSolicitud;
+    }
+
+    public double getMontoSolicitud() {
+        return montoSolicitud;
+    }
+
+    public void setMontoSolicitud(double montoSolicitud) {
+        this.montoSolicitud = montoSolicitud;
+    }
+
+    public Vehiculo getVehiculoSolicitud() {
+        return vehiculoSolicitud;
+    }
+
+    public void setVehiculoSolicitud(Vehiculo vehiculoSolicitud) {
+        this.vehiculoSolicitud = vehiculoSolicitud;
+    }
+
+    public ListaDeVehiculosDisponiblesParaAlquilar_SolicitudDeAlquiler getListaDeVehiculos() {
+        return listaDeVehiculos;
+    }
+
+    public void setListaDeVehiculos(ListaDeVehiculosDisponiblesParaAlquilar_SolicitudDeAlquiler listaDeVehiculos) {
+        this.listaDeVehiculos = listaDeVehiculos;
+    }
+    
     
     
     
